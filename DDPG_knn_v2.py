Implement FLANN for NN search, with reward normalization, fix some bugs.

DDPG_knn_v2.py
@@ -15,7 +15,7 @@ import pyflann
 np.random.seed(1)
 #data = pd.read_csv('ml-latest-small/ratings.csv', header=0, names=['u_id', 'i_id', 'rating', 'timestep'])
 data = pd.read_table('ml-1m/ml-1m/ratings.dat', sep='::',  names=['u_id', 'i_id', 'rating', 'timestep'])
-#data = data[:100]
+# data = data[:100]
 user_idx = data['u_id'].unique()  # id for all the user
 np.random.shuffle(user_idx)
 train_id = user_idx[:int(len(user_idx) * 0.8)]
@@ -57,7 +57,7 @@ def get_movie_idx(input_id):
 # 根据item_id找出相应的动作
 dim = 3  # DDPG-KNN映射的空间维度
 axis = []
-points_in_each_axis = np.ceil(len(movie_id) ** (1.0 / dim))+1
+points_in_each_axis = np.ceil(len(movie_id) ** (1.0 / dim)) + 1
 for i in range(dim):
     axis.append(list(np.linspace(0, 1, points_in_each_axis)))
 
@@ -74,14 +74,26 @@ def normalize(rating):
     return -1 + 2 * (rating - min_rating) / (max_rating - min_rating)
 
 
+def one_hot_rating(input_rating):
+    '''
+    convert the rating to one-hot code.
+    :param input_rating:
+    :return: one-hot code for ratings
+    '''
+    output_rating = np.zeros(11)
+    index = int(input_rating / 0.5)
+    output_rating[index] = 1
+    return output_rating
+
+
 MAX_SEQ_LENGTH = 32
-agent = DDPG(state_dim=128 + 1, action_dim=int(dim), action_bound=1,
-             max_seq_length=MAX_SEQ_LENGTH, batch_size=128, discount_factor=1)
+agent = DDPG(state_dim=128 + 11, action_dim=int(dim), action_bound=1,
+             max_seq_length=MAX_SEQ_LENGTH, batch_size=64, discount_factor=1,actor_lr=1e-4,critic_lr=1e-4)
 
 print('Start training.')
-start_time = datetime.datetime.now()
+start = datetime.datetime.now()
 # 根据训练数据对DDPG进行训练。
-global_step = 0
+train_step = 0
 user_count = 0
 actor_loss_list = []
 critic_loss_list = []
@@ -92,7 +104,7 @@ for id1 in train_id:
     action = []
     for _, row in record.iterrows():  # 针对每个用户的评分数据，对state进行录入
         movie_feature = get_feature(row['i_id'])  # 用户的movie feature
-        current_state = np.hstack((movie_feature.flatten(), row['rating']))
+        current_state = np.hstack((movie_feature.flatten(), one_hot_rating(row['rating'])))
         state.append(current_state)
         reward.append(row['rating'])
         current_movie_idx = get_movie_idx(row['i_id'])
@@ -112,28 +124,30 @@ for id1 in train_id:
             next_state = next_state[-MAX_SEQ_LENGTH:]
             next_state_length = MAX_SEQ_LENGTH
         done = 0
-        if i % 32 is 0:
+        if i % 32 == 0 or i == len(state) - 1:
             done = 1
         agent.store(current_state, current_state_length, current_action, current_reward, next_state,
                     next_state_length, done)
-    memory_length = agent.replay_buffer.get_size()
-    user_count += 1
-    if memory_length > 100:
-        a_loss, c_loss = agent.train(int(memory_length / 32))
-        print('Learning step ', global_step)
-        print('User #:', user_count, '/', len(train_id))
-        print('Actor loss: ', a_loss)
-        print('Critic loss: ', c_loss)
-        actor_loss_list.append(a_loss)
-        critic_loss_list.append(c_loss)
-        global_step += 1
-        agent.replay_buffer.clear()
+        a_loss, c_loss = [], []
+        memory_length = agent.replay_buffer.get_size()
+        if memory_length > 100:
+            a, c = agent.train(1)
+            a_loss.append(a)
+            c_loss.append(c)
+    print('Learning step ', user_count)
+    print('User #:', user_count, '/', len(train_id))
+    print('Actor loss: ', np.mean(a_loss))
+    print('Critic loss: ', np.mean(c_loss))
+    # actor_loss_list.append(a_loss)
+    # critic_loss_list.append(c_loss)
+    train_step += 1
+    user_count+=1
+    agent.replay_buffer.clear()
 
 print('Training finished.')
-end_time = datetime.datetime.now()
-print('Training time(seconds):', (end_time - start_time).seconds)
-pickle.dump(actor_loss_list, open('actor_loss_v2', mode='wb'))
-pickle.dump(critic_loss_list, open('critic_loss_v2', mode='wb'))
+end = datetime.datetime.now()
+training_time = (end - start).seconds
+
 
 def evaluate(recommend_id, item_id, rating, top_N):
     '''
@@ -163,17 +177,20 @@ def evaluate(recommend_id, item_id, rating, top_N):
             output_reward += normalize(current_rating)
             rank = current_recommend_id.index(current_item)
             mrr += 1.0 / (rank + 1)
-    recall = recommend_relevant / relevant if relevant is not 0 else 0
-    precision = recommend_relevant / selected if selected is not 0 else 0
+    recall = recommend_relevant / relevant if relevant != 0 else 0
+    precision = recommend_relevant / selected if selected != 0 else 0
     return output_reward / session_length, precision, recall, mrr / session_length
+
 
 print('Begin test.')
 
-start_time = datetime.datetime.now()
+start = datetime.datetime.now()
 # TEST阶段
+total_testing_steps = 0
 result = []
 ratio = 0.1
-K = int(np.ceil(ratio*len(movie_id)))
+#K = int(np.ceil(ratio * len(movie_id)))
+K = 1
 N = 30  # top-N evaluation
 test_count = 0
 for idx1 in test_id:  # 针对test_id中的每个用户
@@ -192,15 +209,16 @@ for idx1 in test_id:  # 针对test_id中的每个用户
         user_rating_list.append(row['rating'])
         current_movie = row['i_id']
         current_state = np.hstack(
-            (get_feature(current_movie).flatten(), row['rating']))  # current state的维度: movie_length+1
+            (get_feature(current_movie).flatten(), one_hot_rating(row['rating'])))  # current state的维度: movie_length+1
         all_state.append(current_state)  # all_state: 所有的state集合的list
         if len(all_state) > 1:  # 针对第二个电影开始推荐
             temp_state = all_state[:-1]  # 当前的特征list,不包含倒数第一个
             if len(temp_state) > MAX_SEQ_LENGTH:
                 temp_state = temp_state[-MAX_SEQ_LENGTH:]
             proto_action = np.float32(agent.get_action(temp_state, len(temp_state)))  # DDPG-knn输出的Proto action
+            total_testing_steps += 1
             # 根据proto_action找K个最近的动作
-            search_idx, _ = flann.nn(space, np.reshape(proto_action, [dim, ]), K, algorithm='kdtree')  # [1, K]
+            search_idx, _ = flann.nn(space, np.reshape(proto_action, [dim, ]), K, algorithm='kmeans')  # [1, K]
             # 评估nearest_index的value
             search_idx = list(search_idx.flatten())
             search_action = [space[i] for i in search_idx]
@@ -210,7 +228,7 @@ for idx1 in test_id:  # 针对test_id中的每个用户
             # 对temp_state进行补0
             temp_length = len(temp_state)
             if len(temp_state) < MAX_SEQ_LENGTH:  # 如果当前的小于max_length，就补0
-                padding_mat = np.zeros([MAX_SEQ_LENGTH - len(temp_state), 128 + 1])
+                padding_mat = np.zeros([MAX_SEQ_LENGTH - len(temp_state), 128 + 11])
                 temp_state = np.vstack((np.array(temp_state), padding_mat))
             eval_state = np.tile(temp_state, [K, 1])
             eval_length = np.tile(temp_length, [K, 1])
@@ -236,13 +254,20 @@ for idx1 in test_id:  # 针对test_id中的每个用户
     print('Reward@30: %.4f, Precision@30: %.4f, Recall@30: %.4f, MRR@30: %4f'
           % (reward_30, precision_30, recall_30, mkk_30))
     result.append([reward_10, precision_10, recall_10, mkk_10, reward_30, precision_30, recall_30, mkk_30])
+end = datetime.datetime.now()
+testing_time = (end - start).seconds
 
+print('###############')
+print('Learning finished')
+print('Total training steps: {}'.format(train_step))
+print('Total learning time: {}'.format(training_time))
+print('Average learning time for each step: {:.5f}'.format(training_time / train_step))
+print('Total testing steps: {}'.format(total_testing_steps))
+print('Total testing time: {}'.format(testing_time))
+print('Average time per decision: {:.5f}'.format(testing_time / total_testing_steps))
 
 pickle.dump(result, open('ddpg_knn', mode='wb'))
 print('Result:')
 display = np.mean(np.array(result).reshape([-1, 8]), axis=0)
 for num in display:
     print('%.5f' % num)
-
-end_time = datetime.datetime.now()
-print('Testing time(seconds):', (end_time - start_time).seconds)